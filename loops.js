eaa6e0e7c7192d17f8420d07c24ecf7395e00cf3
yashbhangale <68957369+yashbhangale@users.noreply.github.com>
2022-07-11 21:03:20 +0530

    Create loops.js

---
+++ loops.js
@@ -0,0 +1,14 @@
+// While loop
+/* var num = 0;
+
+while(num < 100) {
+    num +=1;
+    console.log(num);
+} */
+
+//for loop
+/*
+for (let num = 0; num <= 100; num++) {
+    console.log(num);
+}
+*/
