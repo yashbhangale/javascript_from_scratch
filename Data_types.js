7a6259af4f59ceafaf3bdaaa8036d9326b058089
yashbhangale <68957369+yashbhangale@users.noreply.github.com>
2022-07-11 21:04:42 +0530

    Create Data_types.js

---
+++ Data_types.js
@@ -0,0 +1,8 @@
+// Data types in Javascript
+let yourAge = 18; // number
+let yourName = 'bob'; //string
+let name = {first:'yashu',last:'bhangale'}; //object
+let truth = false; //boolean
+let groceries = ['apple','banana','orange']; //array
+let random; //undefined
+let nothing = null; //value null
