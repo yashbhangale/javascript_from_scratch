537bc1365caca887af8d2fbc9e2f805404058608
yashbhangale <68957369+yashbhangale@users.noreply.github.com>
2022-07-11 21:01:57 +0530

    Create numbers.js

---
+++ numbers.js
@@ -0,0 +1,12 @@
+// Numbers in Javascript
+var num1=10;
+// Increment num1 by1
+num1 ++;
+// Decrement num1 by1
+num1--;
+console.log(num1);
+// Divide,multiply*,remainder%
+console.log(num1%6);
+// Increment/decrement by any number you want
+num1+=10;
+console.log(num1); 
