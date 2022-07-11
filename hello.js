6e2ea45fec5fddf74b5aa163dc1f5abd1d66c720
yashbhangale <68957369+yashbhangale@users.noreply.github.com>
2022-07-11 21:01:24 +0530

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,10 @@
+console.log('hello');
+alert('hello this is yash');
+// How to write a comment inline
+// Variables
+var b='smoothie';
+console.log(b);
+var someNumber=45;
+console.log(someNumber);
+var age=prompt('What is your age?');
+// document.getElementById('someText').innerHTML=age;
