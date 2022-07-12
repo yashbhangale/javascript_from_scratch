5a576f811d7a4cedb6d2391039353e8aa28b7427
yashbhangale <68957369+yashbhangale@users.noreply.github.com>
2022-07-12 13:11:24 +0530

    Create conditionals.js

---
+++ conditionals.js
@@ -0,0 +1,13 @@
+//conditionals , control flows (if else) 
+// && AND
+// || OR
+
+
+var age = prompt('What is your age?');
+if ( (age >= 18) && (age <= 35) ) {
+    status = 'target demo';
+    console.log(status);
+} else {
+    status = 'not target demo';
+    console.log(status);
+}
